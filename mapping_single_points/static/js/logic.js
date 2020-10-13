4af31e6ba1c1b32c9627a56f3c920e5768360af9
Carolina <caro.negrelli@gmail.com>
2020-10-12 20:51:08 -0400

    dark map with yellow circle

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40.7, -94.5], 14);
 
 // // Create the map object with a center and zoom level. Other way
 // let map = L.map("mapid", {
@@ -26,13 +26,35 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
 
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// para que sea un circulo:
+// L.circle([34.0522, -118.2437], {
+//     radius: 100 //meters
+//  }).addTo(map);
+ // otra forma de hacerlo:
+ L.circleMarker([34.0522, -118.2437], {
+     radius: 300, //pixels
+     color: 'black',
+     fillColor: '#ffffa1'
+ }).addTo(map);
+
+
 //other option:
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+// otro estilo
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: API_KEY
+    accessToken: API_KEY   
 });
+
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
